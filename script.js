a179691d2534076c17951b5237dd82f6c992377d
Daniel Avelino <daniel.avelinoneto@gmail.com>
2026-03-20 11:46:03 -0300

    feat: melhora botão de tema e ajustes no layout

--- script.js
+++ script.js
@@ -8,6 +8,11 @@
 const button = document.querySelector("#toggle-theme");
 const categorias = document.getElementsByClassName("categorias");
 button.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
-    button.classList.toggle("white")
+  document.documentElement.classList.toggle("dark");
+  button.classList.toggle("white")
+  button.textContent =
+    document.documentElement.classList.contains("dark")
+      ? "Modo Claro"
+      : "Modo Escuro";
 });
+
