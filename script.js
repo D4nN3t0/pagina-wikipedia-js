239551ac6fec6bab275f6c8687e3965798b069bc
Daniel Avelino <daniel.avelinoneto@gmail.com>
2026-03-20 11:20:55 -0300

    feat: página estilo wikipedia com modo escuro

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+  Projeto: Wikipedia
+  Autor: Daniel Neto (D4nN3t0)
+  GitHub: https://github.com/D4nN3t0
+  Data: 2026
+*/
+
+const button = document.querySelector("#toggle-theme");
+const categorias = document.getElementsByClassName("categorias");
+button.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    button.classList.toggle("white")
+});
